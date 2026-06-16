74551d4f05a96022995e63c3d7cbb77158956db1
Armando Colone <armandocolone@gmail.com>
2026-06-16 23:04:18 +0200

    feat: Added team member views.
    Views added (list, form, details, search) for project.team.member model.
    Added ir.model.access.csv

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Milestones Module",
-    'version': "1.0.0.0",
+    "version": "18.0.1.0.0",
     'summary': """Modulo per la gestione delle milestones di progetti.""",
     'description': """Il modulo è un'estensione del modulo Projects. Permette la gestione delle milestones e dei relativi task.""",
     'author': "Armando Colone",
@@ -8,9 +8,10 @@
     "depends": ["project"],
     'sequence': 1,
     'data': [
+        "security/ir.model.access.csv",
+        "views/project_team_member_views.xml",
     ],
     'application': True,
-    'auto_install': True,
     'installable': True,
     'license': "LGPL-3"
 }
